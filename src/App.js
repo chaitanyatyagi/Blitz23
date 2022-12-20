409cd236a95ceba82636ac60ff27845216035523
Harshit <2021ucp1382@mnit.ac.in>
2022-12-21 00:11:57 +0530

    Registration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-// import Navbar from './components/Navbar';
-import MainPage from './components/MainPage';
+//import MainPage from './components/MainPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from "react";
+import Registration from './components/Registration';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <MainPage />
+        <Registration/>
       </div>
     </BrowserRouter>
   );
