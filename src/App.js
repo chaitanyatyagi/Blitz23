36c741b5b42ef29dca1e2df23ffcd039d5903247
Harshit <2021ucp1382@mnit.ac.in>
2022-12-21 20:08:43 +0530

    Profile page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 //import MainPage from './components/MainPage';
-import Login from './components/Login'
+//import Login from './components/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from "react";
-import Registration from './components/Registration';
+import Profile from './components/Profile';
+//import Registration from './components/Registration';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Login/>
+        <Profile/>
       </div>
     </BrowserRouter>
   );
