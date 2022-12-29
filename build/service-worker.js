6bc4938b75ff2c68c2b1e55fb264009f28ac703c
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2022-12-29 18:14:36 +0530

    react-scripts added

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Blitzschlag/precache-manifest.8432f3c671b975f1a9ae24cf439497de.js"
+  "/chaitanyatyagi/precache-manifest.97dd11590579580333845ae0b2f6a538.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Blitzschlag/index.html", {
+workbox.routing.registerNavigationRoute("/chaitanyatyagi/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
