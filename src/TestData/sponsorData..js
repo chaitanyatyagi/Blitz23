4e72a15be4e42549f90d52b5c0383094d9bd72c4
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2023-01-01 15:12:16 +0530

    yml update

---
+++ src/TestData/sponsorData..js
@@ -0,0 +1,3 @@
+export default [
+    "1.png", "2.png", "3.jpeg", "4.png", "5.jpeg", "6.png", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg", "12.jpeg", "13.jpeg", "14.png", "15.png", "16.png", "17.png"
+]
