517d8d38ca83a699783d0bdbc06d4e8c62ba2581
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-01-24 17:27:27 +0530

    fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,54 +1,48 @@
-import React from 'react'
-import '../style/contact.css'
-import Contactdetails from './ContactApi'
-import { FaWhatsappSquare, FaInstagramSquare } from 'react-icons/fa';
+import React from "react";
+import "../style/contact.css";
+import Contactdetails from "./ContactApi";
+import { FaWhatsappSquare, FaInstagramSquare } from "react-icons/fa";
 
 export default function Contact() {
-
-    return (
-        <>
-
-            <div className='maincontainer'>
-                <div className='contheading'>
-                    contact us
-                </div>
-                <div className='contnav'>
-                    {Contactdetails.map((item) => {
-                        return (
-                            <div key={item.id} > <a href={"#" + item.id + "id"}>{item.title}</a></div>
-                        )
-                    })}
-                </div>
-                <div className='contdetails'>
-                    {Contactdetails.map((item) => {
-                        return (
-                            <div className='subheading' id={item.id + "id"} key={item.id}>
-                                {item.title}
-                                <div className='cardContainer'>
-                                    {
-                                        item.team.map((member) => {
-                                            return (
-                                                <div className='teamMemb'>
-                                                    <img src={`${member.memimg}`} alt="contimg/azad.jpng" className='contimg' />
-                                                    <div>
-                                                        <p className='memname'>{member.name}</p>
-                                                        <p className='post'>{member.position}</p>
-                                                        <p className='emailc'>{member.email}</p>
-                                                        <p className='phone'>{member.phone}</p>
-                                                    </div>
-
-                                                </div>
-                                            )
-                                        })
-                                    }
-
-                                </div>
-                            </div>
-                        )
-                    })}
-
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="maincontainer">
+				<div className="contheading">contact us</div>
+				<div className="contnav">
+					{Contactdetails.map((item) => {
+						return (
+							<div key={item.id}>
+								{" "}
+								<a href={"#" + item.id + "id"}>{item.title}</a>
+							</div>
+						);
+					})}
+				</div>
+				<div className="contdetails">
+					{Contactdetails.map((item) => {
+						return (
+							<div className="subheading" id={item.id + "id"} key={item.id}>
+								{item.title}
+								<div className="cardContainer">
+									{item.team.map((member) => {
+										return (
+											<div className="teamMemb">
+												<img src={`${member.memimg}`} className="contimg" />
+												<div>
+													<p className="memname">{member.name}</p>
+													<p className="post">{member.position}</p>
+													<p className="emailc">{member.email}</p>
+													<p className="phone">{member.phone}</p>
+												</div>
+											</div>
+										);
+									})}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/HowToRegister.jsx
+++ src/components/HowToRegister.jsx
@@ -1,33 +1,34 @@
-import React from "react"
-import ReactPlayer from 'react-player'
-import "../style/howtoregister.css"
+import React from "react";
+import "../style/howtoregister.css";
 
 export default function HowToRegister() {
-
-    return (
-        <div className="howtoregister">
-            <div className="how2regins how2reginsmar">
-                <div className="how2reginshead">How to create an account in the website?</div>
-                {/* <ReactPlayer url='https://video.blitzschlag.co.in/video/id_video_6/_manifest.mpd' width="70%" height="100%" playing controls className="video-div" /> */}
-                <video src="/video/1.mp4" playing controls className="video-div"></video>
-            </div>
-            <div className="how2regins">
-                <div className="how2reginshead">How to register in an event (for MNIT Jaipur & IIIT Kota students)?</div>
-                <video src="/video/2.mp4" playing controls className="video-div"></video>
-                {/* <ReactPlayer url='https://video.blitzschlag.co.in/video/id_video_4/_manifest.mpd' width="70%" height="100%" playing controls className="video-div" /> */}
-            </div>
-            <div className="how2regins">
-                <div className="how2reginshead">How to register in an event (for NON MNIT Jaipur & NON IIIT Kota students in DESKTOP)?</div>
-                {/* <ReactPlayer url='https://video.blitzschlag.co.in/video/id_video_2/_manifest.mpd' width="70%" height="100%" playing controls className="video-div" /> */}
-                <video src="/video/3.mp4" playing controls className="video-div"></video>
-            </div>
-            <div className="how2regins">
-                <div className="how2reginshead">How to register in an event (for NON MNIT Jaipur & NON IIIT Kota students in ANDROID)?</div>
-                {/* <ReactPlayer url='https://video.blitzschlag.co.in/video/id_video_3/_manifest.mpd' width="70%" height="100%" playing controls className="video-div" /> */}
-                <video src="/video/4.mp4" playing controls className="video-div"></video>
-            </div>
-            {/* <ReactPlayer url='http://159.223.90.95:5000/video/id_video_6/_manifest.mpd' controls /> */}
-        </div>
-    )
-
+	return (
+		<div className="howtoregister">
+			<div className="how2reginsmar"></div>
+			<div className="how2regins">
+				<div className="how2reginshead">How to create an account in the website?</div>
+				<video src="/video/1.mp4" playing controls className="video-div"></video>
+			</div>
+			<div className="how2regins">
+				<div className="how2reginshead">
+					How to register in an event (for MNIT Jaipur & IIIT Kota students)?
+				</div>
+				<video src="/video/2.mp4" playing controls className="video-div"></video>
+			</div>
+			<div className="how2regins">
+				<div className="how2reginshead">
+					How to register in an event (for NON MNIT Jaipur & NON IIIT Kota students in
+					DESKTOP)?
+				</div>
+				<video src="/video/3.mp4" playing controls className="video-div"></video>
+			</div>
+			<div className="how2regins">
+				<div className="how2reginshead">
+					How to register in an event (for NON MNIT Jaipur & NON IIIT Kota students in
+					ANDROID)?
+				</div>
+				<video src="/video/4.mp4" playing controls className="video-div"></video>
+			</div>
+		</div>
+	);
 }
